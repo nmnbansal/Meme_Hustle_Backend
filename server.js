76422f9b7843140f268916e02118b193180de80a
bansalnmn <bansalnmn@gmail.com>
2025-06-20 00:00:03 +0530

    Final Project Origin Cors

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://meme-hustle-frontend-weld.vercel.app/'],
     methods: ['GET', 'POST'],
   },
 });
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173','https://meme-hustle-frontend-weld.vercel.app/'],
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type'],
 }));
